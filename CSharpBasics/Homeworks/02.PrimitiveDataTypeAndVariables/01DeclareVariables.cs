495a43697f3c438191d4c4adf1dd42a9a4e6d2fb
Trifon Apov <TrifonApov@users.noreply.github.com>
2014-11-18 17:58:17 +0200

    Create 01DeclareVariables.cs

---
+++ CSharpBasics/Homeworks/02.PrimitiveDataTypeAndVariables/01DeclareVariables.cs
@@ -0,0 +1,19 @@
+using System;
+
+class DeclareVariables
+{
+    static void Main()
+    {
+        ushort firstNum = 52130;
+        sbyte secondNum = - 115;
+        int thirdNum = 4825932;
+        byte fourthNum = 97;
+        short fifthNum = -10000;
+
+        Console.WriteLine(firstNum + " - ushort");
+        Console.WriteLine(secondNum + " - sbyte");
+        Console.WriteLine(thirdNum + " - int");
+        Console.WriteLine(fourthNum + " - byte");
+        Console.WriteLine(fifthNum + " - short");
+    }
+}
