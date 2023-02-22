ae9eacc0ba336d1ac50643a951568ca3bd77c946
TrifonApov <trifonapov@gmail.com>
2023-02-22 17:00:24 +0200

    Solved Problem 03 - Players and Monsters

--- 2023-02-CSharp-OOP/01-Inheritance-Exercise/PlayersAndMonsters/StartUp.cs
+++ 2023-02-CSharp-OOP/01-Inheritance-Exercise/PlayersAndMonsters/StartUp.cs
@@ -1,10 +1,33 @@
-﻿namespace PlayersAndMonsters
+﻿using System;
+
+namespace PlayersAndMonsters
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
+            MuseElf museElf = new("Pesho", 1);
+            Elf elf = new("Elfina", 2);
 
+            SoulMaster soulMaster = new("Merlin", 3);
+            DarkWizard darkWizard = new("Gosho", 4);
+            Wizard wizard = new("Madafaka", 5);
+
+            BladeKnight bladeKnight = new("Blade", 6);
+            DarkKnight darkKnight = new("Bruce", 7);
+            Knight knight = new("Alfred", 999);
+
+            Hero hero = new("No more heroes anymore", int.MaxValue);
+
+            Console.WriteLine(museElf);
+            Console.WriteLine(elf);
+            Console.WriteLine(soulMaster);
+            Console.WriteLine(darkWizard);
+            Console.WriteLine(wizard);
+            Console.WriteLine(bladeKnight);
+            Console.WriteLine(darkKnight);
+            Console.WriteLine(knight);
+            Console.WriteLine(hero);
         }
     }
 }
