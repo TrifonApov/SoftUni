6f8caa07e73ca48d44b0c93269a798932d4d5a60
TrifonApov <trifonapov@gmail.com>
2023-02-24 21:57:49 +0200

    Solved Encapsulation Lab

---
+++ 2023-02-CSharp-OOP/02-Encapsulation-Lab/PersonsInfo/Person.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace PersonsInfo;
+
+public class Person
+{
+	private string firstName;
+	private string lastName;
+    private int age;
+    private decimal salary;
+
+    public Person(string firstName, string lastName, int age, decimal salary)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+        this.Salary = salary;
+    }
+
+    public string FirstName
+	{
+		get => firstName;
+        private set
+        {
+            if (value.Length < 3)
+                throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+            
+            firstName = value;
+        }
+    }
+
+
+    public string LastName
+    {
+        get => lastName;
+        private set
+        {
+            if (value.Length < 3)
+                throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+            
+            lastName = value;
+        }
+    }
+
+    public int Age
+    {
+        get => age;
+        private set
+        {
+            if (value <= 0)
+                throw new ArgumentException("Age cannot be zero or a negative integer!");
+
+            age = value;
+        }
+    }
+    
+    public decimal Salary
+    {
+        get => salary;
+        private set
+        {
+            //if (value < 650.0M)
+            //    throw new ArgumentException("Salary cannot be less than 650 leva!");
+
+            salary = value;
+        }
+    }
+
+    public void IncreaseSalary(decimal percentage)
+    {
+        if (this.Age < 30)
+            this.Salary += (percentage/2)/100 * this.Salary;
+        else
+            this.Salary += percentage / 100 * this.Salary;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+    }
+}
---
+++ 2023-02-CSharp-OOP/02-Encapsulation-Lab/PersonsInfo/Team.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace PersonsInfo;
+
+public class Team
+{
+    private string name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    public Team(string name)
+    {
+        this.name = name;
+        firstTeam = new List<Person>();
+        reserveTeam = new List<Person>();
+    }
+    public ReadOnlyCollection<Person> FirstTeam => firstTeam.AsReadOnly();
+
+    public ReadOnlyCollection<Person> ReserveTeam => reserveTeam.AsReadOnly();
+
+    public void AddPlayer(Person person)
+    {
+        if (person.Age< 40)
+            firstTeam.Add(person);
+        else
+            reserveTeam.Add(person);
+    }
+
+    public override string ToString()
+    {
+        return
+            $"First team has {firstTeam.Count} players.{Environment.NewLine}Reserve team has {reserveTeam.Count} players.";
+    }
+}
