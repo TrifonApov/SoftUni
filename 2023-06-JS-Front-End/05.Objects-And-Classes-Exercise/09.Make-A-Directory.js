70099322344a37797367ce416d2dbcabd26b372e
Trifon Apov <trifonapov@gmail.com>
2023-07-14 16:05:36 +0300

    Object and Classes Exercises

--- 2023-06-JS-Front-End/05.Objects-And-Classes-Exercise/09.Make-A-Directory.js
+++ 2023-06-JS-Front-End/05.Objects-And-Classes-Exercise/09.Make-A-Directory.js
@@ -1,6 +1,26 @@
-function solve () {
+function solve (input) {
+    const dictionary = input.reduce(
+        (acc, currentTerm) => {
+            let term = Object.entries(JSON.parse(currentTerm));
+            acc[term[0][0]] = term[0][1];
+            return acc;
+        }, {})
     
+    const ordered = Object.keys(dictionary).sort().reduce(
+        (obj, key) => { 
+            obj[key] = dictionary[key]; 
+            return obj;
+        }, {});
+
+    Object.entries(ordered).forEach(term =>{
+        console.log(`Term: ${term[0]} => Definition: ${term[1]}`);
+    });
 }
 
 
-solve();
+solve(['{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+    '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+    '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+    '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
+    '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}']);
+
