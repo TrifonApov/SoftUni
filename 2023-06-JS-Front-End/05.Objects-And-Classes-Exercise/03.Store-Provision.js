6f0117232d5a68c69e1a26040d8c278e14fcfb70
Trifon Apov <trifonapov@gmail.com>
2023-08-09 11:42:53 +0300

    Some exercises

--- 2023-06-JS-Front-End/05.Objects-And-Classes-Exercise/03.Store-Provision.js
+++ 2023-06-JS-Front-End/05.Objects-And-Classes-Exercise/03.Store-Provision.js
@@ -1,23 +1,35 @@
-function solve (storeInput, orderInput) {
+function solve(storeInput, orderInput) {
     let store = {};
 
-    for (let i = 0; i < storeInput.length; i+=2){
-        store[storeInput[i]] = Number(storeInput[i+1]);
+    for (let i = 0; i < storeInput.length; i += 2) {
+        store[storeInput[i]] = Number(storeInput[i + 1]);
     }
-    
-    for (let i = 0; i < orderInput.length; i+=2){
-        if(store.hasOwnProperty(orderInput[i])){
-            store[orderInput[i]] += Number(orderInput[i+1]);
+
+    for (let i = 0; i < orderInput.length; i += 2) {
+        if (store.hasOwnProperty(orderInput[i])) {
+            store[orderInput[i]] += Number(orderInput[i + 1]);
         } else {
-            store[orderInput[i]] = Number(orderInput[i+1]);        
+            store[orderInput[i]] = Number(orderInput[i + 1]);
         }
     }
 
-    Object.entries(store).forEach((product) => console.log(`${product[0]} -> ${product[1]}`))
+    Object.entries(store).forEach((product) =>
+        console.log(`${product[0]} -> ${product[1]}`)
+    );
 }
 
-
 solve(
-    ['Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'],
-    ['Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30']
+    ["Chips", "5", "CocaCola", "9", "Bananas", "14", "Pasta", "4", "Beer", "2"],
+    [
+        "Flour",
+        "44",
+        "Oil",
+        "12",
+        "Pasta",
+        "7",
+        "Tomatoes",
+        "70",
+        "Bananas",
+        "30",
+    ]
 );
