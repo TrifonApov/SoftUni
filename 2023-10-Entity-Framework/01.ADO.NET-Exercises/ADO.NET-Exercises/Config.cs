be8b687a0ea78e816252102c57dca28444dcc2d8
TrifonApov <trifonapov@gmail.com>
2023-10-21 13:11:06 +0300

    ADO.Net Exercise

---
+++ 2023-10-Entity-Framework/01.ADO.NET-Exercises/ADO.NET-Exercises/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace ADO.NET_Exercises
+{
+    internal static class Config
+    {
+        public const string ConnectionSting =
+            @"Server=TRIFON\Trifon;Database=MinionsDB;Integrated Security=True;Trusted_Connection=True";
+    }
+}
